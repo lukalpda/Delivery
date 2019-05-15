00608256741e70b4a2a2a961628410dab15aa021
lucas <lukalpda@gmail.com>
2019-05-15 17:22:55 -0300

    primer commit del delivery

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private afsAuth: AngularFireAuth) {  }
+
+  registerUser(email: string, password: string){
+    return new Promise((resolve, reject)=>{
+      this.afsAuth.auth.createUserWithEmailAndPassword(email, password)
+        .then(userData => resolve(userData),
+          err => reject(err));
+    });
+  }
+
+  loginEmailUser(email: string, password: string){
+    return new Promise((resolve, reject) =>{
+      this.afsAuth.auth.signInWithEmailAndPassword(email,password)
+        .then( userData => resolve(userData),
+          err => reject(err));
+    })
+  }
+
+  logoutUser(){
+    this.afsAuth.auth.signOut();
+  }
+
+  isAuth(){
+    return this.afsAuth.authState.pipe(map(auth => auth));
+  }
+
+}
