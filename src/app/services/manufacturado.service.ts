c0bcd984dd1394c7abe738bc784231386cf3cc29
pablo <psyrap07@gmail.com>
2019-06-02 20:58:58 -0300

    Ultimos cambios del domingo

--- src/app/services/manufacturado.service.ts
+++ src/app/services/manufacturado.service.ts
@@ -10,7 +10,7 @@ export class ManufacturadoService {
 
   constructor(private http: HttpClient) { }
   
-  Url= "http://localhost:8000/api/v1/manufacturados/";
+  Url= "http://localhost:8080/api/v1/manufacturados/";
 
   listarManufacturados(){
     return this.http.get<Manufacturado[]>(this.Url)
