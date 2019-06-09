8c7801643f817a7626cc608134cb72b4c2b049ab
lukalpda <lukalpda@gmail.com>
2019-06-09 15:56:54 -0300

    cliente

--- src/app/components/clientes/agregar-cliente/agregar-cliente.component.ts
+++ src/app/components/clientes/agregar-cliente/agregar-cliente.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from "@angular/core";
 import { Cliente } from "src/app/interfaces/cliente.interface";
 import { Router } from "@angular/router";
 import { ClienteService } from "src/app/services/cliente.service";
+import {Domicilio} from '../../../interfaces/domicilio.interface';
+import {DomicilioService} from '../../../services/domicilio.service';
 
 @Component({
   selector: "app-agregar-cliente",
@@ -10,20 +12,31 @@ import { ClienteService } from "src/app/services/cliente.service";
 })
 export class AgregarClienteComponent implements OnInit {
   clientePost: Cliente;
+  domicilioPost: Domicilio;
   constructor(
     private router: Router,
-    private _agregarClienteService: ClienteService
+    private _agregarClienteService: ClienteService,
+    private _agregarDomicilioService: DomicilioService
   ) {
     //@ts-ignore
     this.clientePost = {};
+    //@ts-ignore
+    this.domicilioPost = {};
   }
 
   ngOnInit() {}
 
   Guardar() {
+
+    this._agregarDomicilioService.crearDomicilio(this.domicilioPost).subscribe(data => {
+      this.clientePost.direccion = data.id_domicilio;
+      console.log("domicilio cargado");
+    });
+
     this._agregarClienteService
       .crearCliente(this.clientePost)
       .subscribe(data => {
+
         alert("Se guardó con éxito");
         this.router.navigate(["clientes"]);
       });
