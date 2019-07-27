c9df491b194b263c3ca547d5d874cfb382a0ef14
lukalpda <lukalpda@gmail.com>
2019-07-27 12:59:21 -0300

    324

--- src/app/components/tienda/carro/carro.component.ts
+++ src/app/components/tienda/carro/carro.component.ts
@@ -7,7 +7,7 @@ import { DetalleVentaService } from "src/app/services/detalle-venta.service";
 import { Router } from "@angular/router";
 import { PedidoService } from "src/app/services/pedido.service";
 import { Pedido } from "src/app/interfaces/pedido.interface";
-import {Cliente} from '../../../interfaces/cliente.interface';
+import {NuevoUsuarioInterface} from '../../../interfaces/nuevo-usuario.interface';
 
 @Component({
   selector: "app-carro",
@@ -20,7 +20,7 @@ export class CarroComponent implements OnInit {
   carroT: DetalleVenta[] = [];
   total: number = 0;
   pedido: Pedido;
-  cliente: Cliente;
+  cliente: NuevoUsuarioInterface;
   pedidos: Pedido[] = [];
   nombreTemporal: string;
   observaciones: string;
@@ -172,6 +172,7 @@ export class CarroComponent implements OnInit {
     if (this.carroT.length > 0) {
       this.pedido.observaciones = this.observaciones;
       this.pedido.nombreTemporal = this.nombreTemporal;
+
       this.pedido.fecha = new Date();
       this.pedido.total = this.total;
       this._pedidoService.crearPedido(this.pedido).subscribe(pedirijillo => {
